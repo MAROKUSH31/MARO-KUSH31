02d3f8e95bf03ba7539dbb74bac9ebaba3770dd6
MAROKUSH31 <liv31tbc@gmail.com>
2026-07-05 23:12:31 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,17 @@
+export default function handler(req, res) {
 
+  if (req.method !== "POST") {
+    return res.status(405).json({ success: false });
+  }
+
+  const { code } = req.body;
+
+  const ACCESS_CODE = "MK312026";
+
+  if (code === ACCESS_CODE) {
+    return res.json({ success: true });
+  }
+
+  return res.json({ success: false });
+
+}
