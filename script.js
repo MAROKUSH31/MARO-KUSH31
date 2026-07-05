776d7e71e27f2ab4db001a76a1d185cf5399727b
MAROKUSH31 <liv31tbc@gmail.com>
2026-07-05 15:03:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const bouton = document.getElementById("entrer");
 
+bouton.addEventListener("click", function () {
+    alert("🚧 Bienvenue ! La boutique premium arrive dans quelques instants...");
+});
