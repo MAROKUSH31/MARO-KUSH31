07c3aa7ac2a15c2b325e129c384a20a0d6a48d8b
MAROKUSH31 <liv31tbc@gmail.com>
2026-07-05 19:00:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -108,3 +108,32 @@ document.addEventListener("click", (e) => {
     }
 
 });
+/* ===========================
+   APPARITION AU SCROLL
+=========================== */
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{
+threshold:.15
+});
+
+document.querySelectorAll(
+".card,.about-card,.contact,.countdown"
+).forEach(el=>{
+
+el.classList.add("fade");
+
+observer.observe(el);
+
+});
